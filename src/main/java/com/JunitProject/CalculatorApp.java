1515afb5b5baa9f8575360ae753a7c19902b309a
SRabaya <srabaya@asaltech.com>
2023-12-06 16:52:39 +0200

    Commit By Shatha

---
+++ src/main/java/com/JunitProject/CalculatorApp.java
@@ -0,0 +1,32 @@
+package com.JunitProject;
+
+public class CalculatorApp {
+
+
+    public int add(int a, int b) {
+
+        return a + b;
+
+    }
+
+    public int subtract(int a, int b) {
+
+        return a - b;
+
+    }
+
+    public int multiply(int a, int b) {
+
+        return a * b;
+
+    }
+
+    public int divide(int a, int b) {
+
+        return a / b;
+
+    }
+
+}
+
+
